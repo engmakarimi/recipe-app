c1a26df0173a0c3827c464beafbbd25b50bdc313
engmakarimi <engineermakarimi@gmail.com>
2023-09-01 20:21:51 +0330

    delete tailwind

--- src/app/shared/components/card/card.component.ts
+++ src/app/shared/components/card/card.component.ts
@@ -1,13 +1,12 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {MatChipsModule} from '@angular/material/chips';
 import { Recipe } from '../../types';
 import { HoverBackgroundColorDirective } from '../../directives';
 
 @Component({
   selector: 'app-card',
   standalone: true,
-  imports: [CommonModule,HoverBackgroundColorDirective,MatChipsModule],
+  imports: [CommonModule,HoverBackgroundColorDirective],
   hostDirectives:[HoverBackgroundColorDirective],
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.scss']
